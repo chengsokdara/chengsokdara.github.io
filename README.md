c21d6eb50796518c6759ca4cea32fa87cb4afe0a
Ra <chengsokdara@gmail.com>
2020-05-24 17:54:24 +0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Cheng Sokdara Portfolio ⚡️
-
-## License 📄
-
-This project is licensed under the GPL-3.0 License - see the [LICENSE](./LICENSE) file for details
---
+++ precache-manifest.abd4b687155fd845bd78981c75423c96.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27610e5e0ead5fd3b5caeb41de26e9ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cce274c2b467974354f",
+    "url": "/static/css/main.1dc0ea7f.chunk.css"
+  },
+  {
+    "revision": "eef39eeebd90c4269c3b",
+    "url": "/static/js/2.d63db901.chunk.js"
+  },
+  {
+    "revision": "7e0fd0c09333f2e525fa479f52af448a",
+    "url": "/static/js/2.d63db901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cce274c2b467974354f",
+    "url": "/static/js/main.91e58a48.chunk.js"
+  },
+  {
+    "revision": "1bd38205388c777ecc7c",
+    "url": "/static/js/runtime-main.e341499a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
